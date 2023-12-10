e84445d1cbc1005efc8a726d04214d559f45628e
Caique Araujo <caique@studiopiggly.com.br>
2023-12-10 17:10:52 -0300

    :package: refactor(lowercase): Always do lowercase to right apply data

--- src/utils/breakify.ts
+++ src/utils/breakify.ts
@@ -1,14 +1,16 @@
 import elements from '@/config/elements';
 
 const breakify = (text: string) => {
-	const found = elements.filter(el => text.startsWith(el));
+	const lowered = text.toLowerCase();
+
+	const found = elements.filter(el => lowered.startsWith(el.toLowerCase()));
 
 	if (found.length === 0) {
 		return [undefined, text];
 	}
 
 	const prefix = found.sort((a, b) => b.length - a.length)[0];
-	return [prefix, text.replace(prefix, '')];
+	return [prefix, lowered.replace(prefix.toLowerCase(), '')];
 };
 
 export default breakify;
